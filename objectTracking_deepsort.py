8dd752a886c9ad25326d8858cfadd5e14b03b0e7
angelazhu96 <amzhu2@illinois.edu>
2024-05-03 14:43:33 -0400

    deepsort saving outputs in a csv

--- objectTracking_deepsort.py
+++ objectTracking_deepsort.py
@@ -21,32 +21,40 @@ def track_objects(deep_sort_config_path):
         nn_budget=cfg.DEEPSORT.NN_BUDGET,
         use_cuda=True
     )
-    vidPath = 'Harpy Data Vehicle/DJI_0406_full.mp4'
+    vidPath = 'Harpy Data Vehicle/DJI_0406_full.MP4'
     video = cv2.VideoCapture(vidPath)
     filePath = 'Harpy Data Vehicle/DJI_0406_tracks.csv'
     data = pd.read_csv(filePath, delimiter=',')
     #print(data)
     bboxes = data.loc[:, ['frame','bboxX','bboxY','Width','Height']]
+    columns_headers = ['x1', 'y1', 'x2', 'y2', 'id', 'frame']
+
     #print(bboxes)
     confidences = torch.ones(bboxes.shape[0])
     #frames = data.loc[:, ['frame']].to_numpy()
    
     tracking_info = []
     #f = open("harpy_deepsort.csv", "w")
-    
+    #outputs = ['x1', 'y1', 'x2', 'y2', 'id', 'frame']
     while video.isOpened():
         ret, frame = video.read()
-        if not ret:
-            break
         current_frame_number = int(video.get(cv2.CAP_PROP_POS_FRAMES)) -1
-        print(current_frame_number)
+        if not ret:
+            break  
+        #print(current_frame_number)
         boxes = bboxes.loc[bboxes['frame']==current_frame_number].values
-        #print(boxes.shape)
-        
-        outputs = deepsort.update(torch.tensor(boxes[:,1:]), confidences[0:len(boxes[0])], frame)
-        tracking_info.append(outputs)
-        print(outputs)
-        np.savetxt("harpy_deepsort.csv", outputs, delimiter=",")
+        if boxes[:,1:].shape[0] > 0:
+            outputs = deepsort.update(torch.tensor(boxes[:,1:]), confidences[0:len(boxes)], frame)
+            for value in outputs:
+                single_box = np.append(value.astype(int),current_frame_number)
+                tracking_info.append(single_box)
+
+    #np.savetxt("harpy_deepsort_cut.csv", columns_headers, delimiter=",")
+    df = pd.DataFrame(tracking_info)
+    #print(df)
+    #print(tracking_info)
+    df.to_csv("harpy_deepsort_full.csv", sep=',', header=columns_headers, index=False)
+    #np.savetxt("harpy_deepsort_cut.csv", tracking_info, delimiter=",")
 
     video.release()
     cv2.destroyAllWindows()
